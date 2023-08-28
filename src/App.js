6c789e3ff268e2ee502de2afb335b7f69a0a985f
401-Nick <95726171+sneaky0129@users.noreply.github.com>
2023-08-28 15:28:42 -0500

    init upload

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>  
+        <nav>
+          <ul>
+            <li>Home</li>
+            <li>About</li>
+            <li>Contact</li>
+          </ul>
+        </nav>
       </header>
+
+      <body>
+        <div className="voidGrid3D">
+            {/* Show the 3d grid here */}
+          </div>
+          <main>
+            <section className="voidGridText">
+              <h1>VoidGrid Transportation Network</h1>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque sodales quam in porta scelerisque. Sed efficitur purus auctor ultricies volutpat. Sed id ex enim. Donec in lectus purus. Ut blandit neque id venenatis viverra. Etiam gravida arcu at magna laoreet aliquet. Pellentesque quis semper augue. Aenean blandit eu lacus at aliquet. In feugiat arcu iaculis nunc ultricies, ut scelerisque velit lacinia. Nullam bibendum at nunc ut vestibulum. Mauris tincidunt, sapien eu semper tempor, nunc ante tincidunt leo, sed viverra ligula ipsum ac lectus.</p>
+              <h2>Header Example</h2>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque sodales quam in porta scelerisque. Sed efficitur purus auctor ultricies volutpat. Sed id ex enim. Donec in lectus purus. Ut blandit neque id venenatis viverra. Etiam gravida arcu at magna laoreet aliquet. Pellentesque quis semper augue. Aenean blandit eu lacus at aliquet. In feugiat arcu iaculis nunc ultricies, ut scelerisque velit lacinia. Nullam bibendum at nunc ut vestibulum. Mauris tincidunt, sapien eu semper tempor, nunc ante tincidunt leo, sed viverra ligula ipsum ac lectus.</p>
+              <h2>Header Example</h2>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque sodales quam in porta scelerisque. Sed efficitur purus auctor ultricies volutpat. Sed id ex enim. Donec in lectus purus. Ut blandit neque id venenatis viverra. Etiam gravida arcu at magna laoreet aliquet. Pellentesque quis semper augue. Aenean blandit eu lacus at aliquet. In feugiat arcu iaculis nunc ultricies, ut scelerisque velit lacinia. Nullam bibendum at nunc ut vestibulum. Mauris tincidunt, sapien eu semper tempor, nunc ante tincidunt leo, sed viverra ligula ipsum ac lectus.</p>
+            </section>
+          </main>
+      </body>
     </div>
   );
 }
